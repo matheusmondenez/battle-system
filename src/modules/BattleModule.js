70c598a0fb416b71197222095f2d924ee830d4af
Matheus Mondenez Paulino <matheus.paulino@kroton.com.br>
2024-09-08 18:10:20 -0300

    fix: fixes the infinite loop when no combat conditions

--- src/modules/BattleModule.js
+++ src/modules/BattleModule.js
@@ -1,8 +1,16 @@
 class BattleModule {
   start(player, enemy) {
+    this.checkCombat(player, enemy)
+
     const [ firstAttacker, secondAttacker ] = this.determineFirstAndSecond(player, enemy)
 
     this.alternateTurn(1, firstAttacker, secondAttacker)
+  }
+
+  checkCombat(player, enemy) {
+    if (player.strength < enemy.defense && enemy.strength < player.defense) {
+      throw new Error('There is no combat due the low level of strength of both characters!')
+    }
   }
 
   determineFirstAndSecond(player, enemy) {
@@ -14,35 +22,39 @@ class BattleModule {
   }
 
   alternateTurn(turn, firstAttacker, secondAttacker) {
-    if (turn % 2 !== 0) {
-      console.info(`Turn ${turn}: ${firstAttacker.name} attacks!`)
-      const damage = this.calculateDamage(firstAttacker, secondAttacker)
-      console.info(`Damage: ${damage}`)
-      secondAttacker.life -= damage
-      console.info(`${secondAttacker.name} has ${secondAttacker.life} life left`)
-      const next = this.checkLife(secondAttacker)
+    setTimeout(() => {
+      if (turn % 2 !== 0) {
+        console.info('--------------------------------------------')
+        console.info(`Turn ${turn}: ${firstAttacker.name} attacks!`)
+        const damage = this.calculateDamage(firstAttacker, secondAttacker)
+        console.info(`Damage: ${damage}`)
+        secondAttacker.life -= damage
+        console.info(`${secondAttacker.name} has ${secondAttacker.life} life left`)
+        const next = this.checkLife(secondAttacker)
 
-      if (next) {
-        turn++
-        this.alternateTurn(turn, firstAttacker, secondAttacker)
+        if (next) {
+          turn++
+          this.alternateTurn(turn, firstAttacker, secondAttacker)
+        } else {
+          return console.info(`${firstAttacker.name} won!`)
+        }
       } else {
-        return console.info(`${firstAttacker.name} won!`)
-      }
-    } else {
-      console.info(`Turn ${turn}: ${secondAttacker.name} attacks!`)
-      const damage = this.calculateDamage(secondAttacker, firstAttacker)
-      console.info(`Damage: ${damage}`)
-      firstAttacker.life -= damage
-      console.info(`${firstAttacker.name} has ${firstAttacker.life} life left`)
-      const next = this.checkLife(firstAttacker)
+        console.info('--------------------------------------------')
+        console.info(`Turn ${turn}: ${secondAttacker.name} attacks!`)
+        const damage = this.calculateDamage(secondAttacker, firstAttacker)
+        console.info(`Damage: ${damage}`)
+        firstAttacker.life -= damage
+        console.info(`${firstAttacker.name} has ${firstAttacker.life} life left`)
+        const next = this.checkLife(firstAttacker)
 
-      if (next) {
-        turn++
-        this.alternateTurn(turn, firstAttacker, secondAttacker)
-      } else {
-        return console.info(`${secondAttacker.name} won!`)
+        if (next) {
+          turn++
+          this.alternateTurn(turn, firstAttacker, secondAttacker)
+        } else {
+          return console.info(`${secondAttacker.name} won!`)
+        }
       }
-    }
+    }, 1000)
   }
 
   calculateDamage(attacker, defender) {
